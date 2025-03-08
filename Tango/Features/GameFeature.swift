b7e90e642bf8b2fb8f065c5f0c6ae0c76a8b32ce
Sergei Vasilenko <sergei.vasilenko@pearson.com>
2025-03-08 19:43:58 +0300

    Added basic layout of a GameView

--- Tango/Features/GameFeature.swift
+++ Tango/Features/GameFeature.swift
@@ -26,10 +26,99 @@ struct GameView: View {
     let store: StoreOf<GameFeature>
 
     var body: some View {
-        NavigationLink(state: LevelsFeature.Path.State.showGameResult(ResultFeature.State(finishedLevel: store.level)))
-        {
-            Text("Finish game")
-            .navigationTitle(Text(store.level.title))
+//        NavigationLink(state: LevelsFeature.Path.State.showGameResult(ResultFeature.State(finishedLevel: store.level)))
+//        {
+//            Text("Finish game")
+//            .navigationTitle(Text(store.level.title))
+//        }
+        ScrollView {
+            VStack {
+                HStack {
+                    Image(systemName: "clock")
+                    Text("0:01")
+                    Spacer()
+                    Button {
+                        print("Clear!")
+                    } label: {
+                        Text("Clear")
+                            .padding(.horizontal, 5)
+                    }
+                    .buttonStyle(.bordered)
+                    .buttonBorderShape(.capsule)
+                }
+                
+                Rectangle()
+                    .foregroundStyle(Color.init(red: 234 / 255.0, green: 227 / 255.0, blue: 217 / 255.0))
+                    .aspectRatio(1, contentMode: .fit)
+                
+                HStack {
+                    Button {
+                        print("Undo!")
+                    } label: {
+                        Text("Undo")
+                            .frame(maxWidth: .infinity)
+                    }
+                    .buttonStyle(.bordered)
+                    .buttonBorderShape(.capsule)
+                    
+                    Button {
+                        print("Hint!")
+                    } label: {
+                        Text("Hint")
+                            .frame(maxWidth: .infinity)
+                    }
+                    .buttonStyle(.bordered)
+                    .buttonBorderShape(.capsule)
+                }
+                
+                DisclosureGroup("How to play") {
+                    VStack(alignment: .leading, spacing: 10) {
+                        HStack {
+                            Text("•")
+                            Text("Fill the grid so that each cell contains either a 🌞 or a 🌚.")
+                        }
+                        
+                        HStack {
+                            Text("•")
+                            Text("No more than 2 🌞 or 🌚 may be next to each other, either vertically or horizontally.")
+                        }
+                        
+                        HStack {
+                            Text("•")
+                            Text("🌞🌞✅")
+                        }
+                        .padding(.leading, 20)
+                        
+                        HStack {
+                            Text("•")
+                            Text("🌞🌞🌞❌")
+                        }
+                        .padding(.leading, 20)
+                        
+                        HStack {
+                            Text("•")
+                            Text("Each row (and column) must contain the same number of 🌞 and 🌚 .")
+                        }
+                        
+                        HStack {
+                            Text("•")
+                            Text("Cells separated by an **=** sign must be of the same type.")
+                        }
+                        
+                        HStack {
+                            Text("•")
+                            Text("Cells separated by an **X** sign must be of the opposite type.")
+                        }
+                        
+                        HStack {
+                            Text("•")
+                            Text("Each puzzle has one right answer and can be solved via deduction (you should never have to make a guess).")
+                        }
+                    }
+                }
+                .frame(width: 300)
+            }
+            .padding(.horizontal, 15)
         }
     }
 }
